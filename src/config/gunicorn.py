986e11df35073497d36d33ee6d6c2234f1277286
narayana108 <narayan-das@github.com>
2021-05-20 18:34:43 +0530

    Change gunicorn workers to 5 + upgraded rdeploy to 0.2.5
    Signed-off-by: narayana108 <narayan-das@github.com>

--- src/config/gunicorn.py
+++ src/config/gunicorn.py
@@ -3,7 +3,7 @@ import os
 
 bind = '0.0.0.0:8000'
 # bind = "127.0.0.1:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = os.environ.get('GUNICORN_WORKERS', 5)
 name = os.environ.get('PROJECT_NAME')
 log_level = 'info'
 log_file = '-'
